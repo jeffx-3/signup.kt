49e80e73dae2ca089ff213ede9932c6cc2d4e42e
Geoffrey Owuor <120029033+jeffx-3@users.noreply.github.com>
2023-03-19 23:42:39 +0300

    Update Signup.kt

--- Signup.kt
+++ Signup.kt
@@ -1,1 +1,62 @@
 
+@Composable
+fun SignUpScreen() {
+    // Initialize state for the form fields
+    val nameState = remember { mutableStateOf("") }
+    val emailState = remember { mutableStateOf("") }
+    val passwordState = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        // Header
+        Text(
+            text = "Sign Up",
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(vertical = 32.dp, horizontal = 16.dp)
+        )
+
+        // Form fields
+        OutlinedTextField(
+            value = nameState.value,
+            onValueChange = { nameState.value = it },
+            label = { Text("Name") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+        OutlinedTextField(
+            value = emailState.value,
+            onValueChange = { emailState.value = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+        OutlinedTextField(
+            value = passwordState.value,
+            onValueChange = { passwordState.value = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+
+        // Sign up button
+        Button(
+            onClick = { /* TODO: Perform sign up action */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 32.dp)
+        ) {
+            Text(text = "Sign Up")
+        }
+    }
+}
